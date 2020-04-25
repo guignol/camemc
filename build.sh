2a334d6807d8939404c3c087be364795f830f744
Sho Sugimoto <17764785+guignol@users.noreply.github.com>
2020-04-25 20:32:17 +0900

    rename

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 #dune exec camer one
-dune exec camer one two three > tmp.s
-gcc -static -o tmp tmp.s
-./tmp
+DIR="_compile"
+mkdir ${DIR}
+dune exec camer one two three > ${DIR}/tmp.s
+gcc -static -o ${DIR}/tmp ${DIR}/tmp.s
+${DIR}/tmp
 echo "$?"
