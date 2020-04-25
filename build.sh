7027ae1cfecb640691cdb1cbed5e404f5b7d5c56
Sho Sugimoto <17764785+guignol@users.noreply.github.com>
2020-04-25 20:25:10 +0900

    emit assembly, link and execute

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-dune exec camer
+#dune exec camer one
+dune exec camer one two three > tmp.s
+gcc -static -o tmp tmp.s
+./tmp
+echo "$?"
--- camer/bin/main.ml
+++ camer/bin/main.ml
@@ -1,2 +1,21 @@
-let () = print_endline "Hello, World!"
-let () = print_endline "Bye!"
+(*let () = print_endline "Hello, World!"*)
+(*let () = print_endline "Bye!"*)
+
+open Printf
+
+let () =
+  for i = 0 to Array.length Sys.argv - 1 do
+    printf "# [%i] %s\n" i Sys.argv.(i)
+  done;;
+
+let () = print_endline ".intel_syntax noprefix"
+let () = print_endline ".text"
+let () = print_endline ".global main"
+let () = print_endline "main:"
+let () = print_endline "  push rbp"
+let () = print_endline "  mov rbp, rsp"
+let () = print_endline "  mov rax, 3"
+let () = print_endline ".Lreturn.main:"
+let () = print_endline "  mov rsp, rbp"
+let () = print_endline "  pop rbp"
+let () = print_endline "  ret"
