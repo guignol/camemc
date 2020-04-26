479dfc0a883fdab59035ab4c1c2ab2dce24b3693
Sho Sugimoto <17764785+guignol@users.noreply.github.com>
2020-04-26 22:24:26 +0900

    prepare tokenizer

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 #dune exec camer one
 DIR="_compile"
 mkdir -p ${DIR}
-dune exec camer one two three four > ${DIR}/tmp.s
+dune exec camer 1 + 2 + 4 > ${DIR}/tmp.s
 gcc -static -o ${DIR}/tmp ${DIR}/tmp.s
 ${DIR}/tmp
 echo "$?"
--- src/bin/main.ml
+++ src/bin/main.ml
@@ -3,11 +3,35 @@
 
 open Printf
 
+type operation = Plus | Minus
+
+type token =
+    Degit of int
+    | Operator of operation
+    | Identifier of string
+
+let tokenize input =
+    match input with
+        ("1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9"|"0") -> Degit (int_of_string input)
+        |"+" -> Operator Plus
+        |"-" -> Operator Minus
+        | _ -> Identifier input
+
+let print_token = function
+    Degit d -> printf "# int: %i\n" d
+    | Operator op ->
+        (match op with
+            Plus -> printf "# operator: %s\n" "+"
+            | Minus -> printf "# operator: %s\n" "-")
+    | Identifier s -> printf "# identifier: %s\n" s
+
 let count = Array.length Sys.argv - 1
-let () =
-  for i = 0 to count do
-    printf "# [%i] %s\n" i Sys.argv.(i)
-  done;;
+
+let rec read i =
+    if i = count then []
+    else tokenize Sys.argv.(i) :: read (i + 1)
+
+let () = List.iter print_token (read 1)
 
 let () = print_endline  ".intel_syntax noprefix"
 let () = print_endline  ".text"
