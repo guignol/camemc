8409d02f100218f3dbee9ac5493c18b44fd8045e
Sho Sugimoto <17764785+guignol@users.noreply.github.com>
2020-05-25 01:48:11 +0900

    calculate stack offset for local variables

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -109,7 +109,7 @@ let operation_of_string = function
 type node =
     | Node_Int of int
     | Node_Binary of operation * node * node
-    | Node_Variable of string * int
+    | Node_Variable of string
     | Node_Assign of node * node
 
 let consume str = function
@@ -192,7 +192,7 @@ and primary tokens = match consume "(" tokens with
     | None -> 
     match consume_identifier tokens with
         | Some (name, tokens) -> 
-            let node = Node_Variable (name, 8 (* TODO *)) in
+            let node = Node_Variable name in
             (node, tokens)
         | None -> expect_int tokens
 
@@ -217,13 +217,18 @@ let parse tokens =
 
 (***********************************************************)
 
+(* https://www.cs.cornell.edu/courses/cs3110/2018sp/l/08-functors/notes.html *)
+module StringMap = Map.Make(String)
+open StringMap
+
 let emit_cmp op =
     print_string    "  cmp rax, rdi\n";
     printf          "  %s al\n" op;
     print_string    "  movzb rax, al\n"
 
-let emit_address = function
-    | Node_Variable (name, offset) ->
+let emit_address var_map = function
+    | Node_Variable name ->
+        let offset = find name var_map in
         printf          "  # variable [%s]\n" name;
         printf          "  lea rax, [rbp - %d]\n" offset;
         print_string    "  push rax\n"
@@ -234,63 +239,95 @@ let load _ =
     (* 8byte == 64bit *)
     print_string    "  mov rax, [rax]\n"
 
-let rec emit = function
-    | Node_Variable _ as v ->
-        emit_address v;
-        load v
-    | Node_Assign (left, right) ->
-        emit_address left;
-        emit right;
-        print_string    "  pop rax\n";
-        print_string    "  pop rdi\n";
-        print_string    "  mov QWORD PTR [rdi], rax\n";
-        print_string    "  push rax\n"
-    | Node_Int d ->
-        printf  "  push %d\n" d;
-        (* TODO returnの代替 *)
-        printf  "  mov rax, %d\n" d
-    | Node_Binary (op, left, right) ->
-        emit left;
-        emit right;
-        print_string    "  pop rdi\n";
-        print_string    "  pop rax\n";
-        begin match op with
-        | PLUS -> print_string  "  add rax, rdi\n";
-        | MINUS -> print_string "  sub rax, rdi\n";
-        | MUL -> print_string   "  imul rax, rdi\n";
-        | DIV ->
-            (*
-            cqo命令を使うと、RAXに入っている64ビットの値を128ビットに伸ばしてRDXとRAXにセットする
-            idivは暗黙のうちにRDXとRAXを取って、それを合わせたものを128ビット整数とみなして、
-            それを引数のレジスタの64ビットの値で割り、商をRAXに、余りをRDXにセットする
-            *)
-            print_string   "  cqo\n";
-            print_string   "  idiv rdi\n";
-        | EQUAL         -> emit_cmp "sete"
-        | NOT_EQUAL     -> emit_cmp "setne"
-        | LESS_THAN     -> emit_cmp "setl"
-        | LESS_EQUAL    -> emit_cmp "setle"
-        | GREATER_THAN  -> emit_cmp "setg"
-        | GREATER_EQUAL -> emit_cmp "setge"
-        end;
-        print_string   "  push rax\n"
+let emit var_map nodes = 
+    let emit_address = emit_address var_map in
+    let rec emit_inner = function
+        | Node_Variable _ as v ->
+            emit_address v;
+            load v
+        | Node_Assign (left, right) ->
+            emit_address left;
+            emit_inner right;
+            print_string    "  pop rax\n";
+            print_string    "  pop rdi\n";
+            print_string    "  mov QWORD PTR [rdi], rax\n";
+            print_string    "  push rax\n"
+        | Node_Int d ->
+            printf  "  push %d\n" d;
+            (* TODO returnの代替 *)
+            printf  "  mov rax, %d\n" d
+        | Node_Binary (op, left, right) ->
+            emit_inner left;
+            emit_inner right;
+            print_string    "  pop rdi\n";
+            print_string    "  pop rax\n";
+            begin match op with
+            | PLUS -> print_string  "  add rax, rdi\n";
+            | MINUS -> print_string "  sub rax, rdi\n";
+            | MUL -> print_string   "  imul rax, rdi\n";
+            | DIV ->
+                (*
+                cqo命令を使うと、RAXに入っている64ビットの値を128ビットに伸ばしてRDXとRAXにセットする
+                idivは暗黙のうちにRDXとRAXを取って、それを合わせたものを128ビット整数とみなして、
+                それを引数のレジスタの64ビットの値で割り、商をRAXに、余りをRDXにセットする
+                *)
+                print_string   "  cqo\n";
+                print_string   "  idiv rdi\n";
+            | EQUAL         -> emit_cmp "sete"
+            | NOT_EQUAL     -> emit_cmp "setne"
+            | LESS_THAN     -> emit_cmp "setl"
+            | LESS_EQUAL    -> emit_cmp "setle"
+            | GREATER_THAN  -> emit_cmp "setg"
+            | GREATER_EQUAL -> emit_cmp "setge"
+            end;
+            print_string   "  push rax\n"
+    in
+    emit_inner nodes
+
+let rec calculate_stack_offset stack m = function
+    | Node_Int _ -> (stack, m)
+    | Node_Variable name ->
+        begin
+        match (find_opt name m) with
+            | Some _ -> (stack, m)
+            | None ->
+                (* TODO 型ごとのサイズ *)
+                let size = 8 in
+                let offset = stack + size in
+                (offset, add name offset m)
+        end
+    | Node_Assign (left, right) -> 
+        let (stack, m) = calculate_stack_offset stack m left in
+        let (stack, m) = calculate_stack_offset stack m right in
+        (stack, m)
+    | Node_Binary (_, left, right) ->
+        let (stack, m) = calculate_stack_offset stack m left in
+        let (stack, m) = calculate_stack_offset stack m right in
+        (stack, m)
+
+let rec stack_offset stack m = function
+    | [] -> (stack, m)
+    | node :: trees -> 
+        let (stack, m) = calculate_stack_offset stack m node in
+        stack_offset stack m trees
 
 let () =
     let input = Sys.argv.(1) in
     let reader index = try Some input.[index] with _ -> None in
     let tokens = tokenize reader in
     let trees = parse tokens in
-    print_endline  ".intel_syntax noprefix";
-    print_endline  ".text";
-    print_endline  ".global main";
-    print_endline  "main:";
+    let (stack, var_map) = stack_offset 0 empty trees in
+    print_endline   ".intel_syntax noprefix";
+    print_endline   ".text";
+    print_endline   ".global main";
+    print_endline   "main:";
     (* プロローグ *)
-    print_endline  "  push rbp";
-    print_endline  "  mov rbp, rsp";
-    print_endline  "  sub rsp, 8"; (* TODO *)
-    List.iter emit trees;
-    print_endline  ".Lreturn.main:";
+    print_endline   "  push rbp";
+    print_endline   "  mov rbp, rsp";
+    printf          "  sub rsp, %d\n" stack;
+    List.iter (emit var_map) trees;
+    print_endline   ".Lreturn.main:";
     (* エピローグ *)
-    print_endline  "  mov rsp, rbp";
-    print_endline  "  pop rbp";
-    print_endline  "  ret";
+    print_endline   "  mov rsp, rbp";
+    print_endline   "  pop rbp";
+    print_endline   "  ret";
