b201705979e13b8326975f8d9d83124fefffb50d
Sho Sugimoto <17764785+guignol@users.noreply.github.com>
2020-07-20 02:00:08 +0900

    refactor

--- src/bin/node.ml
+++ src/bin/node.ml
@@ -5,9 +5,9 @@ type operation = PLUS | MINUS | MUL | DIV
 type 'meta node =
     | Nop
     | Int of int
-	| SizeOf of		'meta node
+    | SizeOf of		'meta node
     | Binary of		'meta * operation * 'meta node * 'meta node
-    | Variable of	'meta * string * int * bool (* is array *)
+    | Variable of	'meta * string * int (* index *) * bool (* is array *)
     | Global of		'meta * string
     | Assign of		'meta * 'meta node * 'meta node
     | Return of		'meta node
@@ -19,24 +19,3 @@ type 'meta node =
     | Address of	'meta node
     | Deref of		'meta * 'meta node
     | Expr_Statement of 'meta node
-
-let convert mm ii node =
-    let rec ff = function
-        | Nop -> Nop
-        | Int		num -> Int num
-		| SizeOf	node ->					SizeOf		(ff node)
-        | Binary	(meta, op, l, r) ->		Binary		(mm meta, op, ff l, ff r)
-        | Variable	(meta, name, i, arr) ->	Variable	(mm meta, name, ii i, arr)
-        | Global	(meta, name) ->			Global		(mm meta, name)
-        | Assign	(meta, l, r) ->			Assign		(mm meta, ff l, ff r)
-        | Return	node ->					Return		(ff node)
-        | If		(c, t, f) ->			If			(ff c, ff t, ff f)
-        | While		(c, e) ->				While		(ff c, ff e)
-        | For		(i, c, iter, e) ->		For			(ff i, ff c, ff iter, ff e)
-        | Block		nodes ->				Block		(List.map ff nodes)
-        | Call		(meta, name, args) ->	Call		(mm meta, name, List.map ff args)
-        | Address	node ->					Address		(ff node)
-        | Deref		(meta, node) ->			Deref		(mm meta, ff node)
-        | Expr_Statement node ->			Expr_Statement (ff node)
-    in
-    ff node
--- src/bin/untyped.ml
+++ src/bin/untyped.ml
@@ -5,37 +5,55 @@ type parameter = {
     offset: int
 }
 
-let size_of_type m = Type.size m
+let untyped_node offset_of_index node =
+    let rec convert = function
+        | Node.Nop -> Node.Nop
+        | Node.Int		num -> Int num
+        | Node.SizeOf	node ->					SizeOf		(convert node)
+        | Node.Binary	(meta, op, l, r) ->		Binary		(Type.size meta, op, convert l, convert r)
+        | Node.Variable	(meta, name, i, arr) ->	Variable	(Type.size meta, name, offset_of_index i, arr)
+        | Node.Global	(meta, name) ->			Global		(Type.size meta, name)
+        | Node.Assign	(meta, l, r) ->			Assign		(Type.size meta, convert l, convert r)
+        | Node.Return	node ->					Return		(convert node)
+        | Node.If		(c, t, f) ->			If			(convert c, convert t, convert f)
+        | Node.While	(c, e) ->				While		(convert c, convert e)
+        | Node.For		(i, c, iter, e) ->		For			(convert i, convert c, convert iter, convert e)
+        | Node.Block	nodes ->				Block		(List.map convert nodes)
+        | Node.Call		(meta, name, args) ->	Call		(Type.size meta, name, List.map convert args)
+        | Node.Address	node ->					Address		(convert node)
+        | Node.Deref	(meta, node) ->			Deref		(Type.size meta, convert node)
+        | Node.Expr_Statement node ->			Expr_Statement (convert node)
+    in
+    convert node
 
 let rec offset_list list sum = function
     | [] -> (list, sum)
     | head :: tail ->
         let { Type.c_type; _} = head in
-        let size = size_of_type c_type in
+        let size = Type.size c_type in
         let sum = sum + size in
         let list = list @ [sum] in
         offset_list list sum tail
 
 let untyped globals = 
-    let rec t converted = function 
+    let rec convert converted = function 
         | [] -> converted
         | global :: globals -> match global with
             | Global.Function ({ Type.name; _}, params, body, locals) ->
                 let (offset_list, stack) = offset_list [] 0 locals in
                 let offset_of_index i = List.nth offset_list i in
-                let un_typed node = Node.convert size_of_type offset_of_index node in
-                let body = List.map un_typed body in
+                let body = List.map (untyped_node offset_of_index) body in
                 let params = List.mapi
                         (fun i { Type.name; Type.c_type; } -> 
-                             let size = size_of_type c_type in
+                             let size = Type.size c_type in
                              let offset = offset_of_index i in
                              { name; size; offset }
                         ) params
                 in
                 let f = Global.Function (name, params, body, stack) in
-                t (converted @ [f]) globals
+                convert (converted @ [f]) globals
             | Global.Variable (c_type, { Type.name; _}) -> 
-                let g = Global.Variable (size_of_type c_type, name) in
-                t (converted @ [g]) globals
+                let g = Global.Variable (Type.size c_type, name) in
+                convert (converted @ [g]) globals
     in
-    t [] globals 
+    convert [] globals 
